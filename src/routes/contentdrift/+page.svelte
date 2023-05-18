0681da69a86dfb46ef3bbcb83415f5eef4012115
Anca Sarb <sarb.anca@gmail.com>
2023-05-18 18:53:07 +0100

    basic gallery with tag filters

--- src/routes/contentdrift/+page.svelte
+++ src/routes/contentdrift/+page.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-	import type { PageData } from "./$types";
+	import type { PageData } from './$types';
 
 	export let data: PageData;
 
 	$: console.log(data);
 </script>
 
-<p class="text-3xl font-bold underline">content drift</p>
+<p>content drift</p>
